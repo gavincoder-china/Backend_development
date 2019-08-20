9c1b435b3730ad756784e609d3e6b14a10692893
gavincoder <907516241@qq.com>
2019-08-20 09:59:39 +0800

    08-120

--- 多线程/Thread_demo/src/Demo03/demo02/ThreadPool/RunnableImpl.java
+++ 多线程/Thread_demo/src/Demo03/demo02/ThreadPool/RunnableImpl.java
@@ -1,10 +1,12 @@
 package Demo03.demo02.ThreadPool;
+
 /*
     2.创建一个类,实现Runnable接口,重写run方法,设置线程任务
  */
-public class RunnableImpl implements Runnable{
+public class RunnableImpl implements Runnable {
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName()+"创建了一个新的线程执行");
+
+        System.out.println(Thread.currentThread().getName() + "创建了一个新的线程执行");
     }
 }
