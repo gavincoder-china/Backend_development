ff2545f254411673757d512d25dab5df2d97769d
gavincoder <907516241@qq.com>
2019-08-23 17:20:40 +0800

    08-23

---
+++ mavendemo/src/main/java/app/controller/userController.java
@@ -0,0 +1,15 @@
+package app.controller;
+
+/**
+ * **********************************************************
+ *
+ * @Project:
+ * @Author : Gavincoder
+ * @Mail : xunyegege@gmail.com
+ * @Github : https://github.com/xunyegege
+ * @ver : version 1.0
+ * @Date : 2019-08-23 16:09
+ * @description:
+ ************************************************************/
+public class userController {
+}
